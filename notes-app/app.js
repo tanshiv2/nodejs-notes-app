99d72d20a87212d4ac6e3085d0cab1670ac22a4a
tanshiv2 <shivangi.tanwar4@gmail.com>
2020-06-19 23:48:14 +0530

    yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,42 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+console.log(process.argv)
+console.log(yargs.argv)
+yargs.version('1.0.1')
+//add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
 
-const mynotes = require('./notes.js');
-const message = mynotes();
+//remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
 
-console.log(message);
-console.log(validator.isEmail('shivangi.tanwar4@gmail.com'))
 
-console.log(validator.isURL('https://isthisvalid.com'));
+yargs.command({
+    command: 'list',
+    describe: 'Lists the notes',
+    handler: function () {
+        console.log('Listing out notes!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'reads the notes',
+    handler: function () {
+        console.log('Reading the notes!')
+    }
+})
+.parse()
+
